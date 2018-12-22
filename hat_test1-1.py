322fe610c7ed34063b45182df759819d221ca927
cgdtaylor <cgdtaylor@live.co.uk>
2018-12-22 21:53:04 +0000

    Add files via upload
    sets a pixel with a random colour in a loop 10 times

--- hat_test1-1.py
+++ hat_test1-1.py
@@ -1,20 +1,25 @@
 #sense hat test 1.1
 from sense_emu import SenseHat
 from random import randint
+from time import sleep 
 sense = SenseHat()
-
-r=randint(0,255)
-g=randint(0,255)
-b=randint(0,255)
-
-sense.set_pixel(0,0,r,g,b)
-
 #display color
+i=0
+while i <=10:
+
+	r=randint(0,255)
+	g=randint(0,255)
+	b=randint(0,255)
+	
+	sense.set_pixel(0,0,r,g,b)
+	i=i+1
+	sleep(2)
+
 
 
 # define scoll speed
-sense.scroll_speed = 15
+#sense.scroll_speed = 15
 
 #send message
-sense.show_message("this is colourful", text_colour=(r,g,b)) 
-print r,g,b
+#sense.show_message("this is colourful", text_colour=(r,g,b)) 
+print i 
