c560e264aaa294a5d92b2945616566b7b96709a7
cgdtaylor <cgdtaylor@live.co.uk>
2018-12-23 17:20:41 +0000

    Update hat_test1-1.py

--- hat_test1-1.py
+++ hat_test1-1.py
@@ -5,21 +5,33 @@ from time import sleep
 sense = SenseHat()
 #display color
 i=0
-while i <=10:
 
-	r=randint(0,255)
-	g=randint(0,255)
-	b=randint(0,255)
+j=0
+
+while 1==1:
+	while j<=7:
+		while i <=7:
+		
+			r=randint(0,255)
+			g=randint(0,255)
+			b=randint(0,255)
+			
+			sense.set_pixel(i,j,r,g,b)
+			i=i+1
+		
+			
+			#if p>=8:
+				#break
+			sleep(0.05)
+		i=0
 	
-	sense.set_pixel(0,0,r,g,b)
-	i=i+1
-	sleep(2)
-
-
-
+		j=j+1
+		
+	j=0
+	
 # define scoll speed
 #sense.scroll_speed = 15
 
 #send message
 #sense.show_message("this is colourful", text_colour=(r,g,b)) 
-print i 
+print(p,j) 
